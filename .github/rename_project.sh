1e3e573a3bb0db511738ebe20146b4ea582e899c
matejkratochvil <matejkratochvil@users.noreply.github.com>
2024-04-21 12:23:12 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="matejkratochvil"
+original_name="moudra"
+original_urlname="moudra"
+original_description="Awesome moudra created by matejkratochvil"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv moudra $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
